9dea0c57bd0986029c9856bbb32499c32e50c5e4
hungmi <hungmihungmi@gmail.com>
2021-07-01 01:25:18 +0800

    bash tail.sh -n LINE

--- tail.sh
+++ tail.sh
@@ -1,2 +1,19 @@
-#! /bin/bash
-ssh -i ~/.ssh/<%= @private_ssh_key %> deploy@<%= @server_ip %> tail -f /home/deploy/<%= @app_name %>/shared/log/production.log
+#!/bin/bash
+
+#trap 'echo $BASH_COMMAND' DEBUG
+
+LINE=30
+
+while getopts ":n:" option
+do
+	case $option in
+		n)
+			echo "n= $OPTARG"
+			LINE="$OPTARG"
+			;;
+	esac
+done
+
+echo $LINE
+
+ssh -i ~/.ssh/<%= @private_ssh_key %> deploy@<%= server_ip %> tail -f /home/deploy/<%= app_name %>/shared/log/production.log -n $LINE
